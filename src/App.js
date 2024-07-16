f2021ba6a4af67382efddc0f4e6a2a9e304deef5
codexpression-nj <n.codexpression@gmail.com>
2024-07-16 23:49:22 +0200

    update

--- src/App.js
+++ src/App.js
@@ -25,8 +25,7 @@ function Game() {
   const moves = history.map((squares, move) => {
     let description;
     if (move == 0) {
-    
-      description = 'Go to game start';
+      description = 'Start';
     }
 
 
